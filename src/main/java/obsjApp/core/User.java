af2336fa33c3ad475a5b9984c7d933c4597c325f
erfanara <m.erfann.ara@gmail.com>
2021-06-17 21:28:39 +0430

    handler is ready for conventions

--- src/main/java/obsjApp/core/User.java
+++ src/main/java/obsjApp/core/User.java
@@ -10,6 +10,8 @@ public class User implements Serializable {
     private String phoneNumber;
     private String email;
     // TODO: password (hash)?? https://duckduckgo.com/?t=ffab&q=how+to+store+password+in+java&ia=web
+//    private String passHash;
+//    private int incorrectPass;
     private final ArrayList<Account> accounts;
 
     public User(String firstname, String lastName, String nationalCode, String phoneNumber, String email) {
--- src/main/java/obsjApp/server/Handler.java
+++ src/main/java/obsjApp/server/Handler.java
@@ -1,16 +1,40 @@
 package obsjApp.server;
 
-import java.io.IOException;
+import netscape.javascript.JSException;
+import netscape.javascript.JSObject;
+
+import java.io.*;
 import java.net.Socket;
 
 public class Handler extends Thread {
-    Socket sock;
+    private Socket sock;
+    PrintWriter out;
+    BufferedReader in;
+
     public Handler(Socket acceptedSocket) throws IOException {
         this.sock = acceptedSocket;
+        in = new BufferedReader(
+                new InputStreamReader(
+                        sock.getInputStream()));
+        // autoFlush enabled
+        out = new PrintWriter(
+                new BufferedWriter(
+                        new OutputStreamWriter(
+                                sock.getOutputStream())), true);
+
+        start();
     }
 
     @Override
     public void run() {
+        try{
 
+        }finally {
+            try {
+                sock.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
