9ccc65a6db69f431a9de1b6621c922b9943c75fa
DRSCrafter <74296624+DRSCrafter@users.noreply.github.com>
2021-06-17 19:21:47 +0430

    Add test for object

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -7,19 +7,39 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
+import java.io.*;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         primaryStage.initStyle(StageStyle.UNDECORATED);
-        Parent root = FXMLLoader.load(getClass().getResource("Test.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 1024, 768));
         primaryStage.show();
     }
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        ObjectOutputStream stream = new ObjectOutputStream(new FileOutputStream("object.java"));
+
+        //Test
+        User reza = new User("Reza", "aezami", "0925", "0915", "dgd@email.com");
+        reza.getAccounts().add(new Account(9111));
+        reza.getAccounts().add(new Account(9112));
+        reza.getAccounts().add(new Account(9113));
+
+        User ali = new User("Ali", "Alian", "0930", "0920", "aba@email.com");
+        ali.getAccounts().add(new Account(9121));
+        ali.getAccounts().add(new Account(9122));
+        ali.getAccounts().add(new Account(9123));
+
+        stream.writeObject(reza);
+        stream.writeObject(ali);
+        stream.close();
+
         launch(args);
     }
 }
