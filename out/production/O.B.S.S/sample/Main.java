1e331e780080ec0fe144c490dcebc02341343750
DRSCrafter <74296624+DRSCrafter@users.noreply.github.com>
2021-06-17 18:45:59 +0430

    Add test for data handling

--- out/production/O.B.S.S/sample/Main.java
+++ out/production/O.B.S.S/sample/Main.java
@@ -21,7 +21,7 @@ public class Main extends Application {
     }
 
 
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
+    public static void main(String[] args) throws IOException {
 
         ObjectOutputStream stream = new ObjectOutputStream(new FileOutputStream("object.java"));
 
