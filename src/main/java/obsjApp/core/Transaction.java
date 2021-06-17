ef5d0287df50ba7c8801593e0426428958af003d
erfanara <m.erfann.ara@gmail.com>
2021-06-17 21:26:24 +0430

    merge of User,Account,Transaction Completed

--- src/main/java/obsjApp/core/Transaction.java
+++ src/main/java/obsjApp/core/Transaction.java
@@ -1,4 +1,34 @@
 package obsjApp.core;
 
-public class Transaction {
+import java.io.Serializable;
+
+public class Transaction implements Serializable {
+    private final char type;
+    private long amount = 0;
+    private final long balance;
+    private String description;
+    private final boolean isSuccessful;
+
+    Transaction(char type, long amount, long balance, boolean isSuccessful) {
+        this.balance = balance;
+        this.type = type;
+        this.amount = amount;
+        this.isSuccessful = isSuccessful;
+    }
+
+    public char getType() {
+        return type;
+    }
+
+    public long getAmount() {
+        return amount;
+    }
+
+    public long getBalance() {
+        return balance;
+    }
+
+    public boolean isSuccessful() {
+        return isSuccessful;
+    }
 }
--- src/main/java/obsjApp/core/User.java
+++ src/main/java/obsjApp/core/User.java
@@ -1,14 +1,47 @@
 package obsjApp.core;
 
-public class User {
-    private String name;
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class User implements Serializable {
+    private String firstname;
     private String lastName;
     private String nationalCode;
     private String phoneNumber;
     private String email;
     // TODO: password (hash)?? https://duckduckgo.com/?t=ffab&q=how+to+store+password+in+java&ia=web
+    private final ArrayList<Account> accounts;
 
+    public User(String firstname, String lastName, String nationalCode, String phoneNumber, String email) {
+        this.firstname = firstname;
+        this.lastName = lastName;
+        this.nationalCode = nationalCode;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        accounts = new ArrayList<>();
+    }
 
+    public String getFirstname() {
+        return firstname;
+    }
 
+    public String getLastName() {
+        return lastName;
+    }
 
+    public String getNationalCode() {
+        return nationalCode;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public ArrayList<Account> getAccounts() {
+        return accounts;
+    }
 }
