dd73518e96e1ec5bcdc52b826e7a4ad8dfc63eaa
erfanara <m.erfann.ara@gmail.com>
2021-06-25 02:15:15 +0430

    a little clean up in Validation class

--- src/main/java/obsjApp/core/Validation.java
+++ src/main/java/obsjApp/core/Validation.java
@@ -1,4 +1,4 @@
-package sample;
+package obsjApp.core;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -6,22 +6,23 @@ import java.util.regex.Pattern;
 // TODO: should we use Apache Commons Validator ? Probably not
 public final class Validation {
     public static boolean isValidEmail(String email) {
+        if (email == null)
+            return false;
+
         String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\." +
                 "[a-zA-Z0-9_+&*-]+)*@" +
                 "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
                 "A-Z]{2,7}$";
 
         Pattern pat = Pattern.compile(emailRegex);
-        if (email == null)
-            return false;
         return pat.matcher(email).matches();
     }
 
-    public static boolean isValidAccountID(String id){
-        Pattern pattern = Pattern.compile("[0-9]", Pattern.CASE_INSENSITIVE);
+    public static boolean isValidAccountID(String id) {
+        if (id == null)
+            return false;
 
-        Matcher matcher = pattern.matcher(id);
-
-        return pattern.matcher(id).matches() && String.valueOf(id).length() == 16;
+        Pattern pat = Pattern.compile("^[1-9][0-9]{15}$");
+        return pat.matcher(id).matches();
     }
 }
