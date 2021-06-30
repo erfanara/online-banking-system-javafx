c6764e711c121859cd4cdcea0c8b11b79ff8c3b8
DRSCrafter <74296624+DRSCrafter@users.noreply.github.com>
2021-06-30 20:51:48 +0430

    Add an initial version of Loan
    We need the class to at least in it's initial state to carry on to finishing other parts

---
+++ src/main/java/obsjApp/core/Loan.java
@@ -0,0 +1,23 @@
+package core;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class Loan {
+
+    private final BigDecimal amount;
+    private final LocalDateTime loan_date;
+
+    Loan(BigDecimal amount) {
+        this.amount = amount;
+        loan_date = LocalDateTime.now();
+    }
+
+    public LocalDateTime getLoan_date() {
+        return loan_date;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+}
