78402a571c092e4abb6df95424947123dd105e78
DRSCrafter <74296624+DRSCrafter@users.noreply.github.com>
2021-06-30 20:40:08 +0430

    Add Dashboard page

---
+++ src/sample/DashboardController.java
@@ -0,0 +1,52 @@
+package Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DashboardController implements Initializable {
+
+    Stage stage;
+    Scene scene;
+    Parent root;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+    @FXML
+    public void SwitchToMain(ActionEvent event) throws IOException {
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        root = FXMLLoader.load(getClass().getResource("Form_Views/Dashboard.fxml"));
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    public void SwitchToServices(ActionEvent event) throws IOException {
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        root = FXMLLoader.load(getClass().getResource("Form_Views/Services.fxml"));
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    public void SwitchToAccountManagement(ActionEvent event) throws IOException {
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        root = FXMLLoader.load(getClass().getResource("Form_Views/AccountManagement.fxml"));
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
