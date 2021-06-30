bbafc1928a96f389f92092855b16229168bf4af4
DRSCrafter <74296624+DRSCrafter@users.noreply.github.com>
2021-06-30 20:50:09 +0430

    Add an initial version of Bill

---
+++ src/main/java/obsjApp/core/Bill.java
@@ -0,0 +1,24 @@
+package core;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class Bill {
+    public enum Type {
+        POWER,
+        WATER,
+        GAS,
+        PHONE
+    }
+
+    private final BigDecimal amount;
+    private LocalDateTime bill_date;
+
+    Bill(BigDecimal amount){
+        this.amount = amount;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+}
