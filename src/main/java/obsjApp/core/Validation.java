dd18a44678e574a48d92c29203d48b514541a28b
DRSCrafter <74296624+DRSCrafter@users.noreply.github.com>
2021-06-30 20:49:27 +0430

    Add a few more validation methods

--- src/main/java/obsjApp/core/Validation.java
+++ src/main/java/obsjApp/core/Validation.java
@@ -1,6 +1,5 @@
-package obsjApp.core;
+package core;
 
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 // TODO: should we use Apache Commons Validator ? Probably not
@@ -24,5 +23,21 @@ public final class Validation {
 
         Pattern pat = Pattern.compile("^[1-9][0-9]{15}$");
         return pat.matcher(id).matches();
+    }
+
+    public static boolean isValidNationalCode(String nationalCode){
+        if (nationalCode == null)
+            return false;
+
+        Pattern pat = Pattern.compile("^([0-9]){10}");
+        return pat.matcher(nationalCode).matches();
+    }
+
+    public static boolean isValidPhoneNumber(String phoneNumber){
+        if (phoneNumber == null)
+            return false;
+
+        Pattern pat = Pattern.compile("^([0-9]){11}");
+        return pat.matcher(phoneNumber).matches();
     }
 }
