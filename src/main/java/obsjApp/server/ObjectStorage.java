93934a5f6f671625917abf3519ab132c43a4dd09
erfanara <m.erfann.ara@gmail.com>
2021-07-02 01:09:13 +0430

    ObjectStorage class in server package added

---
+++ src/main/java/obsjApp/server/ObjectStorage.java
@@ -0,0 +1,36 @@
+package obsjApp.server;
+
+import obsjApp.core.User;
+
+import java.io.*;
+
+public final class ObjectStorage {
+    private final File outputDir;
+
+    public ObjectStorage(String outputDir) {
+        this.outputDir = new File(outputDir);
+    }
+
+    public void writeUser(User user) {
+        try {
+            ObjectOutputStream out =
+                    new ObjectOutputStream(
+                            new FileOutputStream(new File(outputDir, user.getNationalCode())));
+            out.writeObject(user);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public User readUser(String nationalCode) throws FileNotFoundException, ClassNotFoundException {
+        try {
+            ObjectInputStream in =
+                    new ObjectInputStream(
+                            new FileInputStream(new File(outputDir, nationalCode)));
+            return (User) (in.readObject());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
