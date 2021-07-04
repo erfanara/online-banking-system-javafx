391af0f25b2dea8c3f8d1fc29d47b4717bbb336b
Arasteh <drsprogramming2020@gmail.com>
2021-07-04 21:07:21 +0430

    Update Main class to meet the needs of the project

--- src/main/java/obsjApp/client/Main.java
+++ src/main/java/obsjApp/client/Main.java
@@ -6,28 +6,40 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
-import obsjApp.core.Account;
-import obsjApp.core.User;
 
-import java.io.*;
+import java.io.IOException;
 
 public class Main extends Application {
+
+    private static Client client;
+
+    static {
+        try {
+            client = new Client();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Client getClient() {
+        return client;
+    }
+
+    public Main() throws IOException {
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         primaryStage.initStyle(StageStyle.UNDECORATED);
-        Parent root = FXMLLoader.load(getClass().getResource("../formViews/Login.fxml"));
-        primaryStage.setTitle("Hello World");
+
+        Parent root = FXMLLoader.load(getClass().getResource("formViews/test.fxml"));
+
         primaryStage.setScene(new Scene(root, 1024, 768));
         primaryStage.show();
     }
 
 
-    public static void main(String[] args) throws IOException {
-
-        ObjectOutputStream stream = new ObjectOutputStream(new FileOutputStream("object.java"));
-        stream.close();
-
+    public static void main(String[] args) {
         launch(args);
     }
 }
